62c03babbe3ca2ec299fc22262e8473059eba402
Leslie Garcia <leslie.garcia2433@gmail.com>
2022-11-06 14:27:10 -0600

    Modified javascript code to be more compact

--- Develop/script.js
+++ Develop/script.js
@@ -6,97 +6,58 @@ var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 //Declarations for variables not confirmed
 var confirmLength = "";
-var validInputs = [];
 
-//Beginning of prompts
-
-//First prompts confirms length of password
+//This is where the function to generate a password begins
 function generatePassword() {
+  //This confirms the length you want the password to be
   var confirmLength = (prompt("How long would you like the password to be? (Between 8-128 characters)"))
 //This will alert the user that their response was an invalid length and to try again  
   if (confirmLength <= 7 || confirmLength >=129) {
     return "That was an invalid response. Please try again!"
   }
-//This section brings up prompts to confirm uppercase, lowercase, numbers and special characters. Minimum of one needs to be selected
-  
+//This declares out valid inputs empty string. The concat will pull the information from the confirmed arrays and combine them if you would like that criteria in the password
+  let validInputs = [];
   //This first section confirms the use of uppercase letters
   var confirmUpper = confirm("Select OK if you would like to use Uppercase Letters");
+  if (confirmUpper === true) {
+    //the concart pulls the arry for the uppercase letters and combines it into the empty arry for 'validInputs' to use for the pasword
+    validInputs = validInputs.concat(upper)
+    //The two alerts below will provide a message of whether your password will or will not use the specificed criteria
+    alert("Your password will contain uppercase letters.")
+  } else {
+    alert("Your password will not contain uppercase letters.")
+  }
   //This confirms the use of lowercase letters
   var confirmLower = confirm("Select OK if you would like to use Lowercase Letters");
+  if (confirmLower === true) {
+    validInputs = validInputs.concat(lower)
+    alert("Your password will contain lowercase letters.")
+  } else {
+    alert("Your password will not contain lowercase letters.")
+  }
   //This  confirms the user of numbers
   var confirmNumbers = confirm("Select OK if you would like to use Numbers");
+  if (confirmNumbers === true) {
+    validInputs = validInputs.concat(numbers)
+    alert("Your password will contain numbers.")
+  } else {
+    alert("Your password will not contain numbers.")
+  }
   //This confirms the use of special characters
   var confirmSpecial = confirm("Select OK if you would like to use Special Characters (!?@#$%&*)");
+  if (confirmSpecial === true) {
+    validInputs = validInputs.concat(special)
+    alert("Your password will contain special characters.")
+  } else {
+    alert("Your password will not contain special characters.")
+  }
 
   //This will alert the user that minimum requirements were not met and to start over
   if (confirmUpper === false && confirmLower === false && confirmNumbers === false && confirmSpecial === false) {
     return "You need to select at least one character type. Please try again!";
   }
-  //Lines 40 through 78 will give different alerts on the password criteria selected
-  if (confirmUpper === true && confirmLower === false && confirmNumbers === false && confirmSpecial === false) {
-    alert("Your password will be " + confirmLength + " characters long and will include uppercase letters only.")
-  }
-  if (confirmUpper === false && confirmLower === true && confirmNumbers === false && confirmSpecial === false) {
-    alert("Your password will be " + confirmLength + " characters long and will include lowercase letters only.")
-  }
-  if (confirmUpper === false && confirmLower === false && confirmNumbers === true && confirmSpecial === false) {
-    alert("Your password will be " + confirmLength + " characters long and will include numbers only.")
-  }
-  if (confirmUpper === false && confirmLower === false && confirmNumbers === false && confirmSpecial === true) {
-    alert("Your password will be " + confirmLength + " characters long and will include special characters only.")
-  }
-  if (confirmUpper === true && confirmLower === true && confirmNumbers === false && confirmSpecial === false) {
-    alert("Your password will be " + confirmLength + " characters long and will include uppercase and lowercase letters only.")
-  }
-  if (confirmUpper === true && confirmLower === false && confirmNumbers === true && confirmSpecial === false) {
-    alert("Your password will be " + confirmLength + " characters long and will include uppercase letters and numbers only.")
-  }
-  if (confirmUpper === true && confirmLower === false && confirmNumbers === false && confirmSpecial === true) {
-    alert("Your password will be " + confirmLength + " characters long and will include uppercase letters and special characters only.")
-  }
-  if (confirmUpper === false && confirmLower === true && confirmNumbers === true && confirmSpecial === false) {
-    alert("Your password will be " + confirmLength + " characters long and will include lowercase letters and numbers only.")
-  }
-  if (confirmUpper === false && confirmLower === true && confirmNumbers === false && confirmSpecial === true) {
-    alert("Your password will be " + confirmLength + " characters long and will include lowercase letters and special characters only.")
-  }
-  if (confirmUpper === false && confirmLower === false && confirmNumbers === true && confirmSpecial === true) {
-    alert("Your password will be " + confirmLength + " characters long and will include numbers and special characters only.")
-  }
-  if (confirmUpper === true && confirmLower === true && confirmNumbers === true && confirmSpecial === false) {
-    alert("Your password will be " + confirmLength + " characters long and will include uppercase letters, lowercase letters and numbers only.")
-  }
-  if (confirmUpper === true && confirmLower === true && confirmNumbers === false && confirmSpecial === true) {
-    alert("Your password will be " + confirmLength + " characters long and will include uppercase letters, lowercase letters and special characters only.")
-  }
-  if (confirmUpper === false && confirmLower === true && confirmNumbers === true && confirmSpecial === true) {
-    alert("Your password will be " + confirmLength + " characters long and will include lowercase letters, numbers and special characters only.")
-  }
-  if (confirmUpper === true && confirmLower === true && confirmNumbers === true && confirmSpecial === true) {
-    alert("Your password will be " + confirmLength + " characters long and will include upppercase letters, lowercase letters, numbers and special characters.")
-  }
 
-  if (confirmUpper === true) {
-    for (var i = 0; i <upper.length; i++) {
-      validInputs.push(upper[i]);
-    }
-  }
-  if (confirmLower === true) {
-    for (var i = 0; i <lower.length; i++) {
-      validInputs.push(lower[i]);
-    }
-  }
-  if (confirmNumbers === true) {
-    for (var i = 0; i <numbers.length; i++) {
-      validInputs.push(numbers[i]);
-    }
-  }
-  if (confirmSpecial === true) {
-    for (var i = 0; i <special.length; i++) {
-      validInputs.push(special[i]);
-    }
-  }
-
+  //This will generate the random password using the above if conditions
   var randomPassword = "";
     for (var i = 0; i < confirmLength; i++) {
       validInputs[
@@ -106,6 +67,7 @@ function generatePassword() {
               Math.floor(Math.random() * validInputs.length)
           ];
       }
+      //This will generate and show the password that meets the selected criteria 
  return randomPassword;
 }
 
@@ -122,5 +84,6 @@ function writePassword() {
 }
 
 // Add event listener to generate button
+
 generateBtn.addEventListener("click", writePassword);
 console.log(generateBtn)
